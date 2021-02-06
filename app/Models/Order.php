e9621fbb1787939701d01bdb7180d4d421ce1b57
austinb847 <austinb847@icloud.com>
2021-02-05 17:42:15 -0800

    create model function scope to get various comments based on category

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -7,4 +7,28 @@ use Illuminate\Database\Eloquent\Model;
 class Order extends Model
 {
     //
+
+    public function scopeGetComments($query, $category)
+    {
+        switch ($category) {
+            case 'candy':
+                return $query->where('comments', 'LIKE', "%candy%");
+            
+            case 'call';
+                return $query->where('comments', 'LIKE', "%call%");
+            
+            case 'referred';
+                return $query->where('comments', 'LIKE', "%referred%");
+            
+            case 'signature';
+                return $query->where('comments', 'LIKE', "%signature%");
+            
+            case 'miscellaneous':
+                return $query->where('comments', 'NOT LIKE', "%candy%" , 'and', 'comments', 'NOT LIKE', "%call%" , 'and', 'comments', 'NOT LIKE', "%referred%", 
+                                     'and', 'comments', 'NOT LIKE', "%signature%");
+            
+            default:
+                break;
+        }
+    }
 }
