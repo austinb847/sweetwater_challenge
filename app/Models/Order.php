a7a7892c6f685c3070a56f79c0125fd2fbc55797
austinb847 <austinb847@icloud.com>
2021-02-08 18:27:24 -0800

    remove scope methods. Moved all querys to controller instead

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -9,36 +9,4 @@ class Order extends Model
     public $timestamps = false;
     //
 
-    public function scopeGetComments($query, $category)
-    {
-        switch ($category) {
-            case 'candy':
-                return $query->where('comments', 'LIKE', "%candy%");
-            
-            case 'call';
-                return $query->where('comments', 'LIKE', "%call%");
-            
-            case 'referred';
-                return $query->where('comments', 'LIKE', "%referred%");
-            
-            case 'signature';
-                return $query->where('comments', 'LIKE', "%signature%");
-            
-            case 'miscellaneous':
-                $exclude_categorys = [
-                    ['comments' ,'not like','%'.'candy'.'%'],
-                    ['comments' ,'not like','%'.'call'.'%'],
-                    ['comments' ,'not like','%'.'referred'.'%'],
-                    ['comments' ,'not like','%'.'signature'.'%'],
-                ];
-                return $query->where($exclude_categorys);
-            default:
-                break;
-        }
-    }
-
-    public function scopeGetExpectedShipDate($query)
-    {
-        return $query->where('comments', 'LIKE', "%expected ship date%");
-    }
 }
